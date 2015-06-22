ffae852d265fe9301996c24da7a8d73b7f539857
NuenoB <enzocerfogli2@hotmail.com>
2015-06-22 06:19:55 -0400

    Se borro los print de consola

--- DCCNews/static/DCCNews/js/counter.js
+++ DCCNews/static/DCCNews/js/counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
  		$("form#formID input[type=text]").each(function(){
 		 	var input = $(this);
-		 	console.log(input);
+		 	//console.log(input);
 		 	if(input.attr('maxLength')<10000){
 		 		input.maxlength({
           		alwaysShow: true,
@@ -12,7 +12,7 @@ $(document).ready(function(){
 		});
 		$("form#formID textarea").each(function(){
 		 	var input = $(this);
-		 	console.log(input);
+		 	//console.log(input);
 		 	if(input.attr('maxLength')<10000){
 		 		input.maxlength({
           		alwaysShow: true,
