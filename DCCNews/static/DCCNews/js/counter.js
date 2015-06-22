b70c4223765b509ad7323122b5953045c81d2d30
NuenoB <enzocerfogli2@hotmail.com>
2015-06-22 06:09:36 -0400

    Se agrego un conteo de caracteres donde se ingrese texto

---
+++ DCCNews/static/DCCNews/js/counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+ 		$("form#formID input[type=text]").each(function(){
+		 	var input = $(this);
+		 	console.log(input);
+		 	if(input.attr('maxLength')<10000){
+		 		input.maxlength({
+          		alwaysShow: true,
+          		showCharsTyped: false,
+ 		        showMaxLength: false
+      		});
+		 	}
+		});
+		$("form#formID textarea").each(function(){
+		 	var input = $(this);
+		 	console.log(input);
+		 	if(input.attr('maxLength')<10000){
+		 		input.maxlength({
+          		alwaysShow: true,
+          		showCharsTyped: false,
+ 		        showMaxLength: false
+      		});
+		 	}
+		});
+});
